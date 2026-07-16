eb079eba85d4512b2c18c82a537cad5c58a6dbf8
naturemat1 <mateopaulna2@gmail.com>
2026-07-15 23:44:52 -0500

    fix(mobile): simplificar metro.config.js - SDK 57 soporta private fields

--- frontend/mobile/metro.config.js
+++ frontend/mobile/metro.config.js
@@ -2,9 +2,4 @@ const {getDefaultConfig} = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Transpile @clerk/clerk-js to remove private class fields (#)
-// that Hermes in Expo SDK 54 doesn't support
-config.resolver.blacklistRE = undefined;
-config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-preset');
-
 module.exports = config;
